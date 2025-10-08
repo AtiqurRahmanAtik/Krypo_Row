7c6467a3a14a72c30c89ed98c0588f7678528623
AtiqurRahmanAtik <atikahmedtanvir84@gmail.com>
2025-10-08 22:50:23 +0600

    Responsive for mobile device using css media Query

--- index.js
+++ index.js
@@ -2,3 +2,14 @@ console.log("Hello, World!");
 console.log("This is a sample Node.js application.");
 
 
+$(document).ready(function(){
+
+    $(".menu").click(function(){
+    // alert('menu click')
+
+    $('.navLinks').toggle(1000);
+    
+})
+
+});
+
